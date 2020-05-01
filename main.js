a60d2e359f865e06bd33150e112bbe579124b920
kayirmak <dzhumabaev.kai@gmail.com>
2020-05-01 21:15:48 +0300

    final commit

--- main.js
+++ main.js
@@ -1,31 +1,5 @@
 $(document).ready(() => {
     
-
-
-    func = (id) => {
-        $(`.item-${id}`).text($(`.inp-change`).val());
-        
-        let trashBtn = $('<button></button>');
-        let changeBtn = $('<button></button>');
-         
-            trashBtn.addClass('fas fa-trash-alt');
-            changeBtn.addClass('fas fa-exchange-alt');
-            
-            $(`.item-${id}`).append(trashBtn);
-            $(`.item-${id}`).append(changeBtn);
-
-            trashBtn.attr('onclick', `trash(${id})`);
-            changeBtn.attr('onclick', `changeTask(${id})`);
-            
-    }
-
-
-
-
-
-
-
-    
    
     let btn = $('#btn-add');
     btn.click(function(){
@@ -35,6 +9,7 @@ $(document).ready(() => {
         let trashBtn = $('<button></button>');
         let changeBtn = $('<button></button>');
         let clearBtn = $('#btn-clear');
+        
         if(inp.val()){
             let id = Date.now();
             
@@ -43,15 +18,21 @@ $(document).ready(() => {
             item.addClass(`item-${id}`);
             item.addClass('clear');
            
-            item
-            item.text(inp.val());
+            let par = $('<p></p>');
+            par.addClass(`par-${id}`);
+            let pik = $('<p></p>');
+            pik.addClass('pik');
+
+            item.append(par);
+            par.append(pik);
+            pik.text(inp.val());
             list.append(item);
             item.append(trashBtn);
             item.append(changeBtn);
 
             trashBtn.attr('onclick', `trash(${id})`);
             changeBtn.attr('onclick', `changeTask(${id})`);
-            item.attr('onclick', `changeStatus(${id}, event)`);
+            pik.attr('onclick', `changeStatus(${id}, event)`);
             clearBtn.attr('onclick', `clearList()`);
             inp.val('');
 
@@ -63,13 +44,20 @@ $(document).ready(() => {
         let btnTrash = $('<button></button>');
         btnTrash.text('Ok');
         btnTrash.addClass('confirm');
-        $(`.item-${id}`).html('<input class = "inp-change">');
+        $(`.item-${id} .par-${id}`).html('<input class = "inp-change">');
         $(`.item-${id}`).append(btnTrash);
         btnTrash.attr('onclick', `ok(${id})`);
+        $(`.item-${id} .fa-exchange-alt`).css('display', 'none');
 
         ok = (id) => {
-            if($('.inp-change').val()) func(id);
+            if($('.inp-change').val()) {
+                $(`.item-${id} .par-${id}`).text($(`.inp-change`).val());
+                $('.confirm').remove();
+                $('.fa-exchange-alt').css('display', 'block');
+                $(`.par-${id}`).attr('onclick', `changeStatus(${id}, event)`);
+            }
             else alert('Заполните поле');
+            
         }
 
     }
@@ -83,7 +71,7 @@ $(document).ready(() => {
     changeStatus = (id, event) => {
         if(event.target.className == 'fas fa-trash-alt' || event.target.className == 'fas fa-exchange-alt' || event.target.className == 'confirm') return
         else
-        $(`.item-${id}`).toggleClass('completed');
+        $(`.par-${id}`).toggleClass('completed');
     }
 
 
@@ -93,11 +81,5 @@ $(document).ready(() => {
         clear.remove();
     }
     
-
-
-
-
-
-
 
 })
